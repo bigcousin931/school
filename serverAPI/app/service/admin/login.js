a8592c1c71013063f7fa7f726fbe777ec5b9ea9e
BigCousin <601041478@qq.com>
2019-11-18 17:55:57 +0800

    去掉打印

--- serverAPI/app/service/admin/login.js
+++ serverAPI/app/service/admin/login.js
@@ -7,7 +7,6 @@ const Service = require('egg').Service;
 class AdminLoginService extends Service {
   async find() {
     // "users" 为test数据库数据表名
-    console.log(this.app)
     const user = await this.app.mysql.query('select * from user', '');
     return { user };
   }
